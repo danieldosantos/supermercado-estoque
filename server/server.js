96fa5ff20e22bf85a6ec74a088a73a3e9ee368f0
danieldosantos <85966300+danieldosantos@users.noreply.github.com>
2025-07-02 22:44:09 -0300

    Add xss-clean middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const xssClean = require('xss-clean');
 const path = require('path');
 const routes = require('./routes');
 const loadEnv = require('../loadEnv');
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(xssClean());
 app.use(session({ cookie: { maxAge: 60 * 60 * 1000 } }));
 
 // Servir arquivos estáticos
